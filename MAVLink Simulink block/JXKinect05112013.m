c404ef8e4c531b91717afcc593ffa77b50d35b56
jed87 <jianxing_87@hotmail.com>
2013-12-04 16:01:52 +0800

    EDITED vicon filter

--- MAVLink Simulink block/JXKinect05112013.m
+++ MAVLink Simulink block/JXKinect05112013.m
@@ -14,13 +14,13 @@ end
 % SubjectName = 'x330'; SegmentName = 'x330';
 
 % X, Y, Z Global Coordinates of the Quad
-Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('jx', 'jx');
+Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('test', 'test');
 ViconPose(1) = Output_GetSegmentGlobalTranslation.Translation(1);  % X-coordinate
 ViconPose(2) = Output_GetSegmentGlobalTranslation.Translation(2);  % Y-coordinate
 ViconPose(3) = Output_GetSegmentGlobalTranslation.Translation(3);  % Z-coordinate
 
 % Euler Angles
-QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('jx', 'jx');
+QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('test', 'test');
 ViconPose(4) = QuadEulerXYZ.Rotation(1); % Roll
 ViconPose(5) = QuadEulerXYZ.Rotation(2); % Pitch
 ViconPose(6) = QuadEulerXYZ.Rotation(3); % Yaw
@@ -48,13 +48,13 @@ ViconPose(12) = QuadEulerXYZ.Rotation(3); % Yaw
 % SubjectName = 'brush'; SegmentName = 'brush';
 
 % X, Y, Z Global Coordinates of the Quad
-Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('draw', 'draw');
+Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('JX2', 'JX2');
 ViconPose(13) = Output_GetSegmentGlobalTranslation.Translation(1);  % X-coordinate
 ViconPose(14) = Output_GetSegmentGlobalTranslation.Translation(2);  % Y-coordinate
 ViconPose(15) = Output_GetSegmentGlobalTranslation.Translation(3);  % Z-coordinate
 
 % Euler Angles
-QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('draw', 'draw');
+QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('JX2', 'JX2');
 ViconPose(16) = QuadEulerXYZ.Rotation(1); % Roll
 ViconPose(17) = QuadEulerXYZ.Rotation(2); % Pitch
 ViconPose(18) = QuadEulerXYZ.Rotation(3); % Yaw
