cb223010d3377c06abfef9c3b137af8594b9af40
ericweiye <eric_weiye@live.com>
2013-12-08 16:22:54 +0800

    Add car declaration and car tracking model

---
+++ MAVLink Simulink block/GetViconPose_car.m
@@ -0,0 +1,43 @@
+function [ViconPose] = GetViconPose(u)
+global MyClient;
+global QuadRotationMatix;
+
+while (MyClient.GetFrame().Result.Value) ~= (Result.Success)
+	fprintf( '.' );
+end 
+
+
+
+% *********************************
+% x330
+% *********************************
+% SubjectName = 'x330'; SegmentName = 'x330';
+
+% X, Y, Z Global Coordinates of the Quad
+Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('MAV1', 'MAV1');
+ViconPose(1) = Output_GetSegmentGlobalTranslation.Translation(1);  % X-coordinate
+ViconPose(2) = Output_GetSegmentGlobalTranslation.Translation(2);  % Y-coordinate
+ViconPose(3) = Output_GetSegmentGlobalTranslation.Translation(3);  % Z-coordinate
+
+% Euler Angles
+QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('MAV1', 'MAV1');
+ViconPose(4) = QuadEulerXYZ.Rotation(1); % Roll
+ViconPose(5) = QuadEulerXYZ.Rotation(2); % Pitch
+ViconPose(6) = QuadEulerXYZ.Rotation(3); % Yaw
+
+Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('car', 'car');
+ViconPose(7) = Output_GetSegmentGlobalTranslation.Translation(1);  % X-coordinate
+ViconPose(8) = Output_GetSegmentGlobalTranslation.Translation(2);  % Y-coordinate
+ViconPose(9) = Output_GetSegmentGlobalTranslation.Translation(3);  % Z-coordinate
+
+% Euler Angles
+QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('car', 'car');
+ViconPose(10) = QuadEulerXYZ.Rotation(1); % Roll
+ViconPose(11) = QuadEulerXYZ.Rotation(2); % Pitch
+ViconPose(12) = QuadEulerXYZ.Rotation(3); % Yaw
+
+
+end 
+
+
+
