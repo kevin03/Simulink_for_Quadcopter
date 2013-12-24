866f6b4b602b326755601760820418f8311f9a14
soonhooi <soonhooi.chiew@gmail.com>
2013-12-24 13:10:31 +0800

    Initial setup of folder for Airshow

--- MAVLink Simulink block/simulink_support_for_kinect_0.6.0/slkinect/Lib/doc_ja/makedoc.m
+++
@@ -1,8 +0,0 @@
-opts.evalCode=false;
-opts.outputDir='.';
-publish('slnid_imaq.m',opts);
-publish('slnid_image.m',opts);
-publish('slnid_depth.m',opts);
-publish('slnid_motion.m',opts);
-publish('slnid_skeleton.m',opts);
-publish('slnid_ir.m',opts);
