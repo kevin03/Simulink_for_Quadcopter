a16c48a7c34b8b25174f841bb44b0546510c8cc0
ericweiye <eric_weiye@live.com>
2014-01-24 01:14:07 +0800

    wireless relay reliable control

--- MAVLink Simulink block/wirelesscntrol/wirelesscntrol.ino
+++ MAVLink Simulink block/wirelesscntrol/wirelesscntrol.ino
@@ -5,42 +5,31 @@ int relay = 13;
 int green = 4;
 int blue = 5;
 //int red = 3;
+int ch6;
+int ch5;
 void setup() {
-    Serial.begin(57600);
+    Serial.begin(9600);
     //Serial.println("Serial conection started, waiting for instructions...");
     
      // set the digital pin as output:
   pinMode(relay, OUTPUT);
   pinMode(green, OUTPUT);
-  pinMode(blue, OUTPUT);  
+  pinMode(blue, OUTPUT); 
+   pinMode(10, INPUT); 
+   pinMode(11, INPUT);
 }
 void loop() 
 {     
-    if(Serial.available() > 0)
-    {
-        number_of_bytes_received = Serial.readBytesUntil (84,data,20); // read bytes (max. 20) from buffer, untill <CR> (13). store bytes in data. count the bytes recieved.
-       data[number_of_bytes_received] = 0; // add a 0 terminator to the char array
-      
-    }
-    bool result = strcmp (data, "OFFRELAY");
-/*    if (result == true)
- {
-  // Serial.println("data does not matches whatever");
-   digitalWrite(relay, LOW);
- } 
-/else 
- {
-  // Serial.println("data match whatever");
-   digitalWrite(relay, HIGH);
- }*/
-if(result == false){
-digitalWrite(relay,HIGH);
-digitalWrite(green,LOW); 
-digitalWrite(blue,HIGH); 
-}
-else
+    ch6 = pulseIn(10, HIGH, 25000);
+    ch5 = pulseIn(11, HIGH, 25000);
+if(ch6<1500)
+{digitalWrite(relay, HIGH);}
+
+if(ch6>1500)
 {
-digitalWrite(green,HIGH);
-digitalWrite(blue,LOW); 
+delay(3000);
+  digitalWrite(relay, LOW);
 }
+
+  delay(100);
 }
